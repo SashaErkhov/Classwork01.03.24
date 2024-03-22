2ff64edd0f5da9bcb1ecef9b5d6e114be0bb72e2
Aleksandr Erkhov <sasori654654@gmail.com>
2024-03-22 12:52:28 +0300

    working of 22.03

--- Classwork01.03.24/main.cpp
+++ Classwork01.03.24/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "dictpara.h"
-#include "list.h"
+#include "bintree.h"
 #include <ctime>
 
 int main(){
@@ -13,13 +13,13 @@ int main(){
 		return 1;
 	}
 	
-	List<dictPara> Dictionary;
+	BinTree<dictPara> Dictionary;
 	dictPara X;
 	int cnt=0;
 	long tic = static_cast<long>(time(NULL));
 	while(inFile.peek()!=EOF ){ // && cnt<10000
 		inFile >> X;
-		Dictionary.push_back(X);
+		Dictionary.insert(X);
 		++cnt;
 	}
 	inFile.close();
@@ -27,57 +27,61 @@ int main(){
 	std::cout<< "Reading from file " << toc-tic << " sec" <<std::endl;
 	std::cout<<"Total: "<< cnt << std::endl;
 	std::cout<<"Size: "<< Dictionary.size() << std::endl;
-	std::cout<<"Capacity: "<< Dictionary.capacity() << std::endl;
-
-	Dictionary.sort();
-/*
-	for(int i=0; i<100 && i<Dictionary.size(); ++i){
-		std::cout<< Dictionary[i];
-	}
-*/
-	std::cout<< "\nFirst 100 words" <<std::endl;
-	cnt = 0;
-	for(auto pos = Dictionary.rbegin(); 
-		pos != Dictionary.rend() && cnt <100; 
-		++pos, ++cnt){
-		std::cout << *pos;
+	std::cout << "Height: " << Dictionary.height() << std::endl;
+	for (auto pos = Dictionary.begin(); pos != Dictionary.end(); ++pos)
+	{
+		std::cout<<*pos;
 	}
 
-	std::cout<< "\nLast 100 words" <<std::endl;
-	cnt = 0;
-	for(auto pos = Dictionary.rbegin(); 
-		pos != Dictionary.rend() && cnt <100; 
-		++pos, ++cnt){
-		std::cout << *pos;
-	}
-
-
-	char buf[128];
-	do {
-		std::cout << "> ";
-		std::cin.getline(buf, 128);
-
-		dictPara what(buf, "");
-/*
-	for(size_t i=0; i<Dictionary.size(); ++i){
-			if (Dictionary[i] == what){
-				std::cout << Dictionary[i];
-			}
-		}
-*/
-		int cnt=1000;
-		auto pos = Dictionary.find(what);
-		while (cnt > 0)
-		{
-			pos = Dictionary.find(what);
-			--cnt;
-		}
-		if (pos!=Dictionary.end()){
-			std::cout<< *pos;
-			Dictionary.erase(pos);
-		}
-	} while (strlen(buf) > 0);
-
-
+//
+//	//std::cout<<"Capacity: "<< Dictionary.capacity() << std::endl;
+//	Dictionary.sort();
+///*
+//	for(int i=0; i<100 && i<Dictionary.size(); ++i){
+//		std::cout<< Dictionary[i];
+//	}
+//*/
+//	std::cout<< "\nFirst 100 words" <<std::endl;
+//	cnt = 0;
+//	for(auto pos = Dictionary.rbegin(); 
+//		pos != Dictionary.rend() && cnt <100; 
+//		++pos, ++cnt){
+//		std::cout << *pos;
+//	}
+//
+//	std::cout<< "\nLast 100 words" <<std::endl;
+//	cnt = 0;
+//	for(auto pos = Dictionary.rbegin(); 
+//		pos != Dictionary.rend() && cnt <100; 
+//		++pos, ++cnt){
+//		std::cout << *pos;
+//	}
+//
+//
+//	char buf[128];
+//	do {
+//		std::cout << "> ";
+//		std::cin.getline(buf, 128);
+//
+//		dictPara what(buf, "");
+///*
+//	for(size_t i=0; i<Dictionary.size(); ++i){
+//			if (Dictionary[i] == what){
+//				std::cout << Dictionary[i];
+//			}
+//		}
+//*/
+//		int cnt=1000;
+//		auto pos = Dictionary.find(what);
+//		while (cnt > 0)
+//		{
+//			pos = Dictionary.find(what);
+//			--cnt;
+//		}
+//		if (pos!=Dictionary.end()){
+//			std::cout<< *pos;
+//			Dictionary.erase(pos);
+//		}
+//	} while (strlen(buf) > 0);
 	return 0;
 }
